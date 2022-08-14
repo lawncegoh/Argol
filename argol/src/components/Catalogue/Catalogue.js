f2036f3579f7adeb5e42370140351840e9f84a63
webDev <lgohjw@gmail.com>
2022-08-14 20:39:23 +0800

    Edited Catalogue

--- argol/src/components/Catalogue/Catalogue.js
+++ argol/src/components/Catalogue/Catalogue.js
@@ -1,79 +1,166 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 import Navbar from '../Navbar';
-import Product from '../Product/Product';
-import Paintbrush1 from '../../assets/paintbrush1.png';
+import './Catalogue.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faPaintBrush
+} from '@fortawesome/free-solid-svg-icons'
 
-const data = [{  
-        cat: '67 113',  
-        size: '1"',
-        qty: "12 doz"  
-    },{  
-        cat: '67 114',  
-        size: '1"',
-        qty: "12 doz"  
-    },{
-        cat: '67 115',  
-        size: '2"',
-        qty: "12 doz"  
-}]
+const paintbrush_Data = [
+    {
+        model: "DeLuxe Flat",
+        imageUrl: require('../../assets/paintbrush1.png'),
+        description: {
+            first: "Suitable for all paints",
+            second: "Ideal for enamel paint",
+            third: "Wood handle",
+            fourth: "Good blend of pure bristles"
+        },
+    },
+    // {
+    //     model: "Classic Flat Paint Brush",
+    //     imageUrl: require('../../assets/paintbrush1.png'),
+    //     description: {
+    //         first: "Suitable for all paints",
+    //         second: "Good blend of pure bristles",
+    //         third: "Ideal for emulsion / latex paint",
+    //         fourth: "Plastic handle highly resistant to solvents and good grip",
+    //         fifth: "Highly recommend for use in line cutting and trimming"
+    //     },
+    // },
+    // {
+    //     model: "DeLuxe Varnish Brush",
+    //     imageUrl: require('../../assets/paintbrush1.png'),
+    //     description: {
+    //         first: "Suitable for Varnish, Lacquer and Stain",
+    //         second: "Special blend of synthetic bristles",
+    //         third: "Highly minimised shredding of bristles",
+    //         fourth: "Ergonomic wood handle for good and tireless grip",
+    //         fifth: "Fine finishing coat"
+    //     },
+    // },
+    // {
+    //     model: "DeLuxe Pointed Brush",
+    //     imageUrl: require('../../assets/paintbrush1.png'),
+    //     description: {
+    //         first: "Suitable for Varnish, Lacquer and Stain",
+    //         second: "Special blend of synthetic bristles",
+    //         third: "Highly minimised shredding of bristles",
+    //         fourth: "Ergonomic double-extrusion handle for comfort and tireless grip",
+    //         fifth: "Fine finishing coat"
+    //     },
+    // },
+]
 
-const varnishbrush = [{  
-    cat: '67 113',  
-    size: '1"',
-    qty: "12 doz"  
-},{  
-    cat: '67 114',  
-    size: '1"',
-    qty: "12 doz"  
-},{
-    cat: '67 115',  
-    size: '2"',
-    qty: "12 doz"  
-}]
+const Container = styled.div`
+width: 70%;
+height: 100%;
+margin: 0rem auto 10rem;
+display: flex;
+flex-direction: column;
+`
 
-const flatpaintbrush = [{  
-    cat: '67 113',  
-    size: '1"',
-    qty: "12 doz"  
-},{  
-    cat: '67 114',  
-    size: '1"',
-    qty: "12 doz"  
-},{
-    cat: '67 115',  
-    size: '2"',
-    qty: "12 doz"  
-}]
+const Model = styled.div`
+width: 100%;
+text-align: left;
+margin-bottom: 4rem;
+`
+
+const Content = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+width: 100%;
+margin: auto;
+`
+
+const Description = styled.div`
+display: flex;
+flex-direction: column;
+width: 30%;
+height: 100%;
+padding: 10px 20px;
+background-color: #edeef0;
+border-radius: 10px;
+letter-spacing: 1px;
+justify-content: center;
+margin-right: 10%;
+`
+
+const Image = styled.img`
+width: 55%;
+height: 60%;
+`
+
+const Category = styled.p`
+font-size: 72px;
+letter-spacing: 10px;
+color: #bdbdbd;
+text-align: center;
+margin-bottom: 4rem;
+`
+
+
+// const Display = styled.div`
+// width: 90%;
+// height: 100vh;
+// margin: auto;
+// position: relative;
+// `
+
+// const Tagline = styled.div`
+// display: flex;
+// flex-direction: column;
+// position: absolute;
+// bottom: 10%;
+// right: 6%;
+// text-align: flex-start;
+// `
+
+// const Background = styled.img`
+// width: 100%;
+// height: 100vh;
+// `
 
 const Catalogue = () => {
     return (
-    <Fragment>
-        <Navbar/>
-        <Product header="DeLuxe Flat Paint Brush" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Varnish Brush" img={Paintbrush1} data={varnishbrush}/>
-        <Product header="Classic Flat Paint Brush" img={Paintbrush1} data={flatpaintbrush}/>
-        <Product header="DeLuxe Pointed Brush" img={Paintbrush1} data={data}/>
-        {/* 8 types of deluxe refills */}
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Range Refills" img={Paintbrush1} data={data}/>
+        <Fragment>
+            {/* <Display>
 
-        {/* 5 types of classic refills */}
-        <Product header="Classic Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="Classic Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="Classic Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="Classic Range Refills" img={Paintbrush1} data={data}/>
-        <Product header="Classic Range Refills" img={Paintbrush1} data={data}/>
+                <Background src={require('../../assets/background.jpg')}/>
 
-        <Product header="DeLuxe Paint Brush" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Paint Brush" img={Paintbrush1} data={data}/>
-        <Product header="DeLuxe Paint Brush" img={Paintbrush1} data={data}/>
-    </Fragment>
+                <Tagline>
+                    <p style={{letterSpacing: '2px', fontSize: '20px'}}>Before You Start,<br/>Think Of The Finish</p>
+                </Tagline>
+            </Display> */}
+            <Category>Brushes</Category>
+            {paintbrush_Data.map((data) => {
+                return (
+                    <Container>
+                        {/* Landing page for the Catalogue */}
+
+                        {/* Model name */}
+                        <Model>
+                            <p style={{fontSize: '26px', letterSpacing: '1.5px'}}>{data.model}</p>
+                        </Model>
+                        {/* Main content (image and description) */}
+                        <Content>
+                            {/* Image */}
+                            <Image src={data.imageUrl} />
+                            {/* Product description */}
+                            <Description>
+                                <p>{data?.description?.first}</p>
+                                <p>{data?.description?.second}</p>
+                                <p>{data?.description?.third}</p>
+                                <p>{data?.description?.fourth}</p>
+                                <p>{data?.description?.fifth}</p>
+                            </Description>
+                        </Content>
+                    </Container>
+                )
+            })}
+        </Fragment>
     )
 }
 
