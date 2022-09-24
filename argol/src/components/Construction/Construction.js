6b687dda229ba1df31a0f417c7544803cf40b616
LanceJw <lgohjw@gmail.com>
2022-09-24 15:24:54 +0800

    Edited "Under Construction" Page

--- argol/src/components/Construction/Construction.js
+++ argol/src/components/Construction/Construction.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Logo1 from '../../assets/logo1.png'
+
 const Container = styled.div`
 width: 100%;
 height: 100vh;
 display: flex;
+flex-direction: column;
 justify-content: center;
 align-items: center;
+padding-bottom: 5rem;
 `
 
 const Content = styled.p`
@@ -18,6 +22,7 @@ text-align: center;
 const Construction = () => {
     return (
         <Container>
+            <img style={{width: '10%', height: '18%'}} src={Logo1} alt='Argol'/>
             <Content>Under Construction</Content>
         </Container>
     )
