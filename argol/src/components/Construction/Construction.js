cbff3b44a82117d32c12e68a5a6ff91b418f0e0a
LanceJw <lgohjw@gmail.com>
2022-09-24 15:29:00 +0800

    Edited mobile construction page

--- argol/src/components/Construction/Construction.js
+++ argol/src/components/Construction/Construction.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import Logo1 from '../../assets/logo1.png'
@@ -19,12 +19,58 @@ font-weight: bold;
 text-align: center;
 `
 
+const MobileContent = styled.p`
+font-size: 22px;
+font-weight: bold;
+text-align: center;
+`
+
 const Construction = () => {
+
+
+    const [state, setState] = useState({
+        mobileView: false,
+    });
+
+    const { mobileView } = state;
+
+    useEffect(() => {
+        const setResponsiveness = () => {
+            return window.innerWidth < 1100
+                ? setState((prevState) => ({ ...prevState, mobileView: true }))
+                : setState((prevState) => ({ ...prevState, mobileView: false }));
+        };
+
+        setResponsiveness();
+        window.addEventListener("resize", () => setResponsiveness());
+
+        return () => {
+            window.removeEventListener("resize", () => setResponsiveness());
+        }
+    }, []);
+
+    const displayDesktop = () => {
+        return (
+            <Container>
+                <img style={{ width: '10%', height: '18%' }} src={Logo1} alt='Argol' />
+                <Content>Under Construction</Content>
+            </Container>
+        )
+    }
+
+    const displayMobile = () => {
+        return (
+            <Container>
+                <img style={{ width: '32%', height: '18%' }} src={Logo1} alt='Argol' />
+                <MobileContent>Under Construction</MobileContent>
+            </Container>
+        )
+    }
+
     return (
-        <Container>
-            <img style={{width: '10%', height: '18%'}} src={Logo1} alt='Argol'/>
-            <Content>Under Construction</Content>
-        </Container>
+        <Fragment>
+            {mobileView ? displayMobile() : displayDesktop()}
+        </Fragment>
     )
 }
 
